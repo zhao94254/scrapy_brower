1f67858ee81d6a38b3925319917a394f60bec641
zhaopengyun <zhaopengyun@funji.club>
2019-05-13 18:41:20 +0800

    代理完善

--- brower.py
+++ brower.py
@@ -6,6 +6,11 @@
 
 import requests
 from logging import getLogger
+from utils import BrowerInfo
+import time
+from twisted.internet.error import (
+    TimeoutError, TCPTimedOutError)
+
 log = getLogger('fakebrower')
 
 def get_proxy(proxy_type):
@@ -16,22 +21,38 @@ class FakeBrower(object):
 
     def __init__(self, settings):
         self.use_proxy = settings.get("USE_PROXY", True) # 是否启用代理，默认开启
+        self.brower_info = BrowerInfo(url=settings.get('BROWERINFO_URL'))
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings)
 
     def process_request(self, request, spider):
+        self.req_start = time.time()
         proxy_type = request.url.split(':')[0]
         if 'proxy' in request.meta:
             if 'exception' in request.meta and not request.meta["exception"]:
                 return
-            request.meta['proxy'] = get_proxy(proxy_type) # 替换
+            request.meta['proxy'] = self.brower_info.get_proxy(proxy_type, spider.name) # 替换
         if self.use_proxy:
-            request.meta['proxy'] = get_proxy(proxy_type) # 初始化获取
+            request.meta['proxy'] = self.brower_info.get_proxy(proxy_type, spider.name) # 初始化获取
+            request.headers['User-Agent'] = self.brower_info.get_ua()
+
+    def process_response(self, request, response, spider):
+        self.brower_info.put_proxy(request.meta['proxy'],
+                                   spider.name,
+                                   1)
+        log.info(f"proxy={request.meta['proxy']} cost={time.time()-self.req_start}")
+        return response
 
     def process_exception(self, request, exception, spider):
         if 'proxy' not in request.meta:
             return
         if exception:
             request.meta['exception'] = True
+            if isinstance(exception, (TimeoutError, TCPTimedOutError)):
+                self.brower_info.del_proxy(request.meta['proxy'], spider.name)
+            else:
+                self.brower_info.put_proxy(request.meta['proxy'],
+                                           spider.name,
+                                           -1)
